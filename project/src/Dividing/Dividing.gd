1c5a6aeb43d0db42f802d893a2d6fe3afc9e4db7
Dominik <zawlockidominik@gmail.com>
2024-12-20 21:38:13 +0100

    Play Correct animation in Dividing screen

--- project/src/Dividing/Dividing.gd
+++ project/src/Dividing/Dividing.gd
@@ -6,11 +6,17 @@ extends Control
 @onready var answer_line_edit = %AnswerLineEdit
 @onready var check_btn = %CheckBtn
 @onready var question_generator = %DivQuestionGenerator
+@onready var correct_answer_popup = $CorrectAnswerPopup
+
+# Used when animatio is playing
+var _ingnore_input: bool = false
 
 
 func _ready() -> void:
 	answer_line_edit.text_submitted.connect(_check)
 	check_btn.pressed.connect(_check)
+	
+	correct_answer_popup.animation_end.connect(_on_animation_end)
 	
 	question_generator.generate_correctness_map(10000)
 	
@@ -27,14 +33,21 @@ func _generate_next_question() -> void:
 
 
 func _check(_v=null) -> void:
+	if _ingnore_input:
+		return
+	
 	var answer_text : String = answer_line_edit.text
 	var answer_int : int = answer_text.to_int()
 	var is_correct = question_generator.check(answer_int)
 	
 	if is_correct:
-		# TODO: animation
-		pass
+		_ingnore_input = true
+		correct_answer_popup.play()
 	else:
 		pass
 	
 	_generate_next_question()
+
+
+func _on_animation_end() -> void:
+	_ingnore_input = false

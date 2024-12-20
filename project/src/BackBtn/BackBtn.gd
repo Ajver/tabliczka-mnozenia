576f3dc2c0ca3141ce3c3812a4fc4bb8af241e2f
Dominik <zawlockidominik@gmail.com>
2024-12-20 19:48:10 +0100

    Better UI

--- project/src/BackBtn/BackBtn.gd
+++ project/src/BackBtn/BackBtn.gd
@@ -1,10 +1,32 @@
 extends MarginContainer
 
-signal pressed
+@export var default_color: Color
+@export var accent_color: Color
+
+@onready var texture_rect: TextureRect = %TextureRect
+@onready var label: Label = %Label
 
 
 func _ready() -> void:
-	$Button.pressed.connect(_back_to_main_menu)
+	var btn : TextureButton = $Button
+	btn.pressed.connect(_back_to_main_menu)
+	btn.mouse_entered.connect(_on_btn_hover_over)
+	btn.mouse_exited.connect(_on_btn_hover_end)
+	btn.button_down.connect(_on_btn_down)
+
+
+func _on_btn_hover_over() -> void:
+	label.modulate = accent_color
+	texture_rect.modulate = accent_color
+
+
+func _on_btn_hover_end() -> void:
+	label.modulate = default_color
+	texture_rect.modulate = default_color
+
+
+func _on_btn_down() -> void:
+	pass
 
 
 func _back_to_main_menu() -> void:
--- project/src/MainMenu/MenuBtn/MenuBtn.gd
+++ project/src/MainMenu/MenuBtn/MenuBtn.gd
@@ -2,6 +2,30 @@ extends MarginContainer
 
 signal pressed
 
+@export var default_color: Color
+@export var accent_color: Color
+
+@onready var texture_rect: TextureRect = %TextureRect
+@onready var label: Label = %Label
+
 
 func _ready() -> void:
-	$Button.pressed.connect(pressed.emit)
+	var btn : TextureButton = $Button
+	btn.pressed.connect(pressed.emit)
+	btn.mouse_entered.connect(_on_btn_hover_over)
+	btn.mouse_exited.connect(_on_btn_hover_end)
+	btn.button_down.connect(_on_btn_down)
+
+
+func _on_btn_hover_over() -> void:
+	label.modulate = accent_color
+	texture_rect.modulate = accent_color
+
+
+func _on_btn_hover_end() -> void:
+	label.modulate = default_color
+	texture_rect.modulate = default_color
+
+
+func _on_btn_down() -> void:
+	pass
